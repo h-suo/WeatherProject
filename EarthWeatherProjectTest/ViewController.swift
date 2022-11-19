914a67659a36bd3bb207733cef9999bfc4918815
h-suo <phstoo1021@naver.com>
2022-11-19 21:16:52 +0900

    FirstViewControllers front Design and ViewController first View Design

--- EarthWeatherProjectTest/ViewController.swift
+++ EarthWeatherProjectTest/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var firstview: UIView!
+    @IBOutlet weak var tempLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        
+        //view를 클릭 가능하도록 설정
+        self.firstview.isUserInteractionEnabled = true
+        //제쳐스 추가
+        self.firstview.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.firstViewTapped)))
     }
-
-
+    
+    func setupUI() {
+        firstview.backgroundColor = .gray
+        firstview.layer.cornerRadius = 8
+    }
+    
+    @objc func firstViewTapped(_ sender: UITapGestureRecognizer) {
+        performSegue(withIdentifier: "showFirstView", sender: nil)
+    }
+    
 }
 
